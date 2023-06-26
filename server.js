8cc63aabe69f9b2e8b8809a6fd2d946f44835bf6
visinodeveloper <visinodeveloper@gmail.com>
2023-06-26 03:43:13 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,81 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const cors = require('cors');
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log('Connected to MongoDB');
+
+})
+
+
+const personSchema = new Schema({
+    name: String,
+    spent: Number,
+    owe: Number,
+})
+
+const Person = mongoose.model('Person', personSchema, "Persons");
+
+
+
+//Middlewares
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+
+//ROUTES
+app.get("/api/personas", async (req, res) => {
+    Person.find().then((persons)=>{
+        res.status(200).json(persons)
+    }
+    )
+})
+
+app.post("/api/personas", async (req, res) => {
+    const person = req.body
+    console.log(person)
+    
+    Person.create({
+        name:person.name,
+        spent:0,
+        owe:0,
+    
+    }).then((createdPerson)=>{
+        res.status(201).json(createdPerson)
+    })
+})
+
+app.put("/api/personas/", async (req, res) => {
+    const person = req.body;
+    console.log(person);
+  
+    try {
+      const updatedPerson = await Person
+      .findByIdAndUpdate(person.id, {
+        $inc: { 
+            spent: parseInt(person.spent),
+             owe: parseInt(person.owe) },
+      });
+  
+      console.log("Finish update");
+      console.log(updatedPerson);
+      res.status(200).json(updatedPerson);
+    } catch (error) {
+      console.error("Error al actualizar los valores:", error);
+      res.status(500).json({ error: "Error al actualizar los valores" });
+    }
+  });
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    
+})
