bd41f63c26300b17e892e31521c295606ac24f26
visinodeveloper <visinodeveloper@gmail.com>
2023-06-26 03:53:00 -0300

    dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const cors = require('cors');
